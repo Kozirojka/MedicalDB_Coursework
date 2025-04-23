f593a1a246564e37fd422a900e21ab1f38e41345
Stalker <vihovaneckostik@gmil.com>
2025-04-24 01:06:10 +0300

    create user request sending

---
+++ MedicApp.Backend/MedicApp.Application/Patient/Command/CreateVisitRequest/CreateVisitRequestCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MedicApp.Domain.Dto.Requests;
+
+namespace MedicApp.Application.Patient.Command.CreateVisitRequest;
+
+public record CreateVisitRequestCommand : IRequest<CreateVisitRequestResponse>
+{
+    public string Description { get; init; }
+    public int PatientId { get; init; }
+
+    public CreateVisitRequestCommand(VisitRequestDto dto, int patientId)
+    {
+        Description = dto.Description;
+        PatientId = patientId;
+    }
+}
+
+
+public record CreateVisitRequestResponse
+{
+    public int? RequestId { get; init; }
+    public string Message { get; init; }
+}
+
