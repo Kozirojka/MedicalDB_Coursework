36939e95be29510eb04e1f24ac2d961ea68ebd62
Stalker <vihovaneckostik@gmil.com>
2025-04-24 23:15:43 +0300

    fix problem

--- MedicApp.Backend/MedicApp.Api/Endpoints/Doctor/GetPendingVisits/GetPendingVisitsEndpoint.cs
+++ MedicApp.Backend/MedicApp.Api/Endpoints/Doctor/GetPendingVisits/GetPendingVisitsEndpoint.cs
@@ -3,28 +3,26 @@ using MedicApp.Domain.Dto.Responce;
 
 namespace MedicApp.Api.Endpoints.Doctor.GetPendingVisits;
 
-public class GetPendingVisitsEndpoint(IMediator mediator) : IEndpoint
+public class GetPendingVisitsEndpoint : IEndpoint
 {
-    
-       
     public void RegisterEndpoints(IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet("/api/v2/doctor/visits/pending-visits", Handler);
     }
 
-    private async Task<IResult> Handler(HttpContext context)
+    private static async Task<IResult> Handler(HttpContext context)
     {
-        
+        var mediator = context.RequestServices.GetRequiredService<IMediator>();
+
         var dto = new DoctorRequestFilterDto 
         { 
             Id = 4,
             Status = "AssignedToDoctor"
         };
 
-        var listOfPendingRequestsCommand = dto.MapToCommand();
-        
-        var result = await mediator.Send(listOfPendingRequestsCommand);
-        
+        var command = dto.MapToCommand();
+        var result = await mediator.Send(command);
+
         return TypedResults.Ok(result);
     }
 }
