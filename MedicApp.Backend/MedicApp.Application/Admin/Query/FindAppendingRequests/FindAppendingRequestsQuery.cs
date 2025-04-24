c226c40d2cf9790a7268359fb8166fda56bea9b0
Stalker <vihovaneckostik@gmil.com>
2025-04-24 22:16:21 +0300

    create appending user

---
+++ MedicApp.Backend/MedicApp.Application/Admin/Query/FindAppendingRequests/FindAppendingRequestsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using MedicApp.Application.Admin.Query.FindAppendingRequests;
+
+namespace MedicalVisits.Application.Admin.Queries.FindAppendingRequests;
+
+public class FindAppendingRequestsQuery : IRequest<List<VisitResponceDto>>
+{
+    
+}
