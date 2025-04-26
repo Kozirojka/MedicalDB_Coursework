0dbd556ab8d81e53c157d470e8dabd0d6eb04b8e
Stalker <vihovaneckostik@gmil.com>
2025-04-26 11:05:34 +0300

    create endpoint for confirmation from side of doctor

---
+++ MedicApp.Backend/MedicApp.Application/Doctor/AssignDoctorToVisit/AssignDoctorToVisitCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using MedicApp.Domain.Dto.Responce;
+
+namespace MedicApp.Application.Doctor.AssignDoctorToVisit;
+
+public class AssignDoctorToVisitCommand : IRequest<AssignmentResult> 
+{
+    public int VisitId { get; }
+    public int DoctorId { get; }
+    public int SlotTimeId { get; set;  }
+    public AssignDoctorToVisitCommand(int visitId, int doctorId, int slotTimeId)
+    {
+        VisitId = visitId;
+        DoctorId = doctorId;
+        SlotTimeId = slotTimeId;
+    }
+}
+
+
