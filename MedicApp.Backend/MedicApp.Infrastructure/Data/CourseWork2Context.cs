5589a4a04b8570ffe6dc3dfbcb9835b604c65bdc
Stalker <vihovaneckostik@gmil.com>
2025-05-07 00:52:57 +0300

    create generation of data for database

--- MedicApp.Backend/MedicApp.Infrastructure/Data/CourseWork2Context.cs
+++ MedicApp.Backend/MedicApp.Infrastructure/Data/CourseWork2Context.cs
@@ -48,7 +48,7 @@ public partial class CourseWork2Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=CourseWork_2;Username=postgres;Password=admin");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=CourseWork_3;Username=postgres;Password=admin");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
